cea0e61f160fc5610e88a10466c470124b8fceea
WithLithum <WithLithum@outlook.com>
2024-10-10 19:49:10 +0800

    🐛 (frontend) Fix long basename

--- src/IceCraft/Frontend/Cli/FrontendUtil.cs
+++ src/IceCraft/Frontend/Cli/FrontendUtil.cs
@@ -10,5 +10,5 @@ public static class FrontendUtil
 {
     internal static readonly Option<bool> OptVerbose = new("--verbose", "Enable verbose output");
     internal static readonly Option<bool> OptDebug = new("--debug", "Allow debugger attach confirmation before acting");
-    internal static readonly string BaseName = Environment.GetCommandLineArgs()[0];
+    internal static readonly string BaseName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
 }
