555ddde79581f72b0ea7ce6b333bf71556e1eb39
WithLithum <WithLithum@outlook.com>
2024-10-26 12:19:44 +0800

    ✅ (artefact) Add various ArtefactManager tests
    Signed-off-by: WithLithum <WithLithum@outlook.com>

--- src/IceCraft.Tests/ArtefactManagerTests.cs
+++ src/IceCraft.Tests/ArtefactManagerTests.cs
@@ -39,6 +39,68 @@ public class ArtefactManagerTests
     }
 
     [Fact]
+    public void CleanArtefacts_RemoveSevenDayLongFiles()
+    {
+        // Arrange
+        var oldDate = DateTime.UtcNow - TimeSpan.FromDays(12);
+        var newDate = DateTime.UtcNow - TimeSpan.FromDays(1);
+        var fs = new MockFileSystem();
+        fs.AddFile("/ic/artefacts/oldFile", new MockFileData("OLD")
+        {
+            CreationTime = oldDate
+        });
+        fs.AddFile("/ic/artefacts/newFile", new MockFileData("NEW")
+        {
+            CreationTime = newDate
+        });
+
+        var manager = new ArtefactManager(_frontendApp, Mock.Of<IChecksumRunner>(), fs);
+
+        // Act
+        manager.CleanArtefacts();
+
+        // Assert
+        Assert.False(fs.FileExists("/ic/artefacts/oldFile"));
+        Assert.True(fs.FileExists("/ic/artefacts/newFile"));
+    }
+
+    [Fact]
+    public void CreateArtefactFile_Hashed_CreateFileAndOpenWritableStream()
+    {
+        // Arrange
+        var fileSystem = new MockFileSystem();
+        var artefactManager = new ArtefactManager(_frontendApp,
+            Mock.Of<IChecksumRunner>(),
+            fileSystem);
+        var artefact = new HashedArtefact("test", "test");
+        var path = artefactManager.GetArtefactPath(artefact, MockMeta);
+
+        // Act
+        using var stream = artefactManager.CreateArtefactFile(artefact, MockMeta);
+
+        // Assert
+        Assert.True(stream.CanWrite);
+        Assert.True(fileSystem.FileExists(path));
+    }
+
+    [Fact]
+    public void CreateArtefactFile_Volatile_Throw()
+    {
+        // Arrange
+        var fileSystem = new MockFileSystem();
+        var artefactManager = new ArtefactManager(_frontendApp,
+            Mock.Of<IChecksumRunner>(),
+            fileSystem);
+        var artefact = new VolatileArtefact();
+
+        // Act
+        var exception = Record.Exception(() => artefactManager.CreateArtefactFile(artefact, MockMeta));
+
+        // Assert
+        Assert.IsType<ArgumentException>(exception);
+    }
+
+    [Fact]
     public void GetArtefactPath_AlwaysNullWhenVolatile()
     {
         // Arrange
@@ -70,6 +132,71 @@ public class ArtefactManagerTests
 
         // Assert
         Assert.NotEqual(resultA, resultB);
+    }
+
+    [Fact]
+    public async Task GetSafeArtefactPathAsync_FileNotExist_ReturnNull()
+    {
+        // Arrange
+        var fileSystem = new MockFileSystem();
+        var artefactManager = new ArtefactManager(_frontendApp,
+            Mock.Of<IChecksumRunner>(),
+            fileSystem);
+        var artefact = new HashedArtefact("test", "test");
+
+        // Act
+        var result = await artefactManager.GetSafeArtefactPathAsync(artefact, MockMeta);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task GetSafeArtefactPathAsync_FileExistAndHashValid_ReturnPath()
+    {
+        // Arrange
+        var fileSystem = new MockFileSystem();
+        var checksumRunner = new Mock<IChecksumRunner>();
+        var artefact = new HashedArtefact("test", "test");
+        checksumRunner.Setup(x => x.ValidateAsync(artefact, It.IsAny<Stream>()))
+            .Returns(Task.FromResult(true));
+
+        var artefactManager = new ArtefactManager(_frontendApp,
+            checksumRunner.Object,
+            fileSystem);
+
+        var realPath = artefactManager.GetArtefactPath(artefact, MockMeta);
+        fileSystem.AddFile(realPath, "DATA");
+
+        // Act
+        var result = await artefactManager.GetSafeArtefactPathAsync(artefact, MockMeta);
+
+        // Assert
+        Assert.Equal(realPath, result);
+    }
+
+    [Fact]
+    public async Task GetSafeArtefactPathAsync_FileExistButHashInvalid_ReturnNull()
+    {
+        // Arrange
+        var fileSystem = new MockFileSystem();
+        var checksumRunner = new Mock<IChecksumRunner>();
+        var artefact = new HashedArtefact("test", "test");
+        checksumRunner.Setup(x => x.ValidateAsync(artefact, It.IsAny<Stream>()))
+            .Returns(Task.FromResult(false));
+
+        var artefactManager = new ArtefactManager(_frontendApp,
+            checksumRunner.Object,
+            fileSystem);
+
+        var realPath = artefactManager.GetArtefactPath(artefact, MockMeta);
+        fileSystem.AddFile(realPath, "DATA");
+
+        // Act
+        var result = await artefactManager.GetSafeArtefactPathAsync(artefact, MockMeta);
+
+        // Assert
+        Assert.Null(result);
     }
 
     [Fact]
