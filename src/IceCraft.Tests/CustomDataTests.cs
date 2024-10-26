6e26a749bc45cb330b6372e6a3289b71e12ae14d
WithLithum <WithLithum@outlook.com>
2024-10-26 10:18:07 +0800

    ✅ (test) Fix custom data testing problems
    Signed-off-by: WithLithum <WithLithum@outlook.com>

--- src/IceCraft.Tests/CustomDataTests.cs
+++ src/IceCraft.Tests/CustomDataTests.cs
@@ -4,27 +4,29 @@
 
 namespace IceCraft.Tests;
 
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
+using IceCraft.Api.Installation.Dependency;
 using IceCraft.Api.Package.Data;
+using IceCraft.Api.Serialization;
+using Newtonsoft.Json;
+using Semver;
 
 public class CustomDataTests
 {
-    private record struct SampleData(int Value);
-
     [Fact]
     public void CustomDataDictionary_Serialized_ValueType()
     {
         // Arrange
         const string key = "test-key";
-        var data = new SampleData(10);
-        var typeInfo = JsonTypeInfo.CreateJsonTypeInfo<SampleData>(JsonSerializerOptions.Default);
+        var data = new DependencyReference("id", SemVersionRange.All);
         
         var dict = new PackageCustomDataDictionary();
-        dict.PutSerialize(key, data, typeInfo);
+        dict.PutSerialize(key, data, IceCraftApiContext.Default.DependencyReference);
         
         // Act
-        var success = dict.TryGetValueDeserialize(key, typeInfo, out var value);
+        var success = dict.TryGetValueDeserialize(key, IceCraftApiContext.Default.DependencyReference, out var value);
         
         // Assert
         Assert.True(success);
